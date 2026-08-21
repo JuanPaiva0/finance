3b6a8285c0e5fb3871d66d83848383091c94d966
Juan Amcis Paiva <juanpaiva887@gmail.com>
2026-08-21 15:35:02 -0300

    add validation for category in transactions

--- backend/app/api/transactions/service.py
+++ backend/app/api/transactions/service.py
@@ -1,12 +1,19 @@
 from app.api.transactions.repository import TransactionRepository
 from app.schemas.Transaction import TransactionCreate, TransactionUpdate
 from app.api.transactions.exceptions import NoFieldsUpdateException, TransactionNotFoundException
+from app.api.categories.repository import CategoryRepository
+from app.api.categories.exceptions import CategoryNotFoundException
 
 class TransactionService:
     def __init__(self):
         self.transaction_repository = TransactionRepository()
+        self.category_repository = CategoryRepository()
 
     async def create_transaction(self, transaction: TransactionCreate, user_id: int):
+        category = await self.category_repository.get_category(transaction.category_id, user_id)
+        if not category:
+            raise CategoryNotFoundException()
+
         return await self.transaction_repository.create_transaction(transaction, user_id)
 
     async def get_transactions(self, user_id: int):
@@ -22,6 +29,11 @@ class TransactionService:
     async def update_transaction(self, transaction_id: int, transaction_update: TransactionUpdate, user_id: int):
         if not transaction_update.model_dump(exclude_unset=True):
             raise NoFieldsUpdateException()
+
+        if transaction_update.category_id is not None:
+            category = await self.category_repository.get_category(transaction_update.category_id, user_id)
+            if not category:
+                raise CategoryNotFoundException()
 
         updated_transaction = await self.transaction_repository.update_transaction(transaction_id, transaction_update, user_id)
         if not updated_transaction:
